70d2ac33972eeb88d497eccf41a3e9d2bf622df3
Clinton <Danielsc05@gmail.com>
2018-11-28 21:46:17 -0500

    make it better

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+    name: "Christian Knight",
+    photo: "https://assets.classicfm.com/2014/24/bad-stock-photos-3-1403270969-view-0.jpg",
+    scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+},
+{
+    name: "Cameron Joyner",
+    photo: "https://www.samaritanmag.com/sites/default/files/resize/images/audiomicro_0-340x340.jpg",
+    scores: [
+        "4",
+        "3",
+        "5",
+        "2",
+        "3",
+        "1",
+        "5",
+        "3",
+        "2",
+        "4"
+    ]
+},
+{
+name: "Samantha Ritz",
+    photo:"https://image.shutterstock.com/image-photo/portrait-smiling-young-black-woman-260nw-562454995.jpg",
+    scores: [
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "4",
+        "2",
+        "3",
+        "5",
+        "2"
+    ]
+},
+{
+    name: "Kevin Smith",
+    photo: "https://static.parade.com/wp-content/uploads/2018/08/College-Snack-Shopping-FTR.jpg", 
+    scores: [
+        "1",
+        "4",
+        "2",
+        "3",
+        "5",
+        "4",
+        "1",
+        "1",
+        "4",
+        "2"
+    ]
+},
+{
+    name: "Tiffany Garcia",
+    photo:"https://t4.ftcdn.net/jpg/00/74/64/01/240_F_74640112_MYMjG1kUG2G40fCyzzEjaHhJtF6D7GqK.jpg",
+    scores: [
+        "5",
+        "4",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4"
+    ]
+}
+];
+
+module.exports = friends;
